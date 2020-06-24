08d601b8db2fcf904418bb1a7fb687d4b1c18ee6
Cloud <hongzezhi_email@126.com>
2020-06-24 17:19:11 +0800

    Joinery Demo

---
+++ src/test/java/com/cloud/basicspringboot/BasicSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.basicspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
